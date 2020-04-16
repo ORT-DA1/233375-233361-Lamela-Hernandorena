9f76d2a348f8e416be8b7c4c1e99c1fecb41b41b
Joaquin Lamela <joaquin.lamela00@gmail.com>
2020-04-16 15:24:02 -0300

    Estructure initial of the project
    Add the projects of Business Logic, UI and Test.

---
+++ Obligatory_SentimentalAnalysis/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5876389e-e4b5-4e53-bcde-2f92cb0d553a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
