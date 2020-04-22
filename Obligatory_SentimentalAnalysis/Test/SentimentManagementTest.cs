4ecaac88df935cf17ee81183913e550c40196439
Joaquin Lamela <joaquin.lamela00@gmail.com>
2020-04-22 18:42:08 -0300

    Adding some logic and test
    We change the type of attribute SentimentType to a enumerate type, and we add test of ToString method and the logic for that.

--- Obligatory_SentimentalAnalysis/Test/SentimentManagementTest.cs
+++ Obligatory_SentimentalAnalysis/Test/SentimentManagementTest.cs
@@ -8,19 +8,22 @@ namespace Test
 	public class SentimentManagementTest
 	{
 
-		SentimentManagement manegement; 
+		SentimentManagement manegement;
+	
 
 		[TestInitialize]
 		public void SetUp()
 		{
 			manegement = new SentimentManagement(); 
+			
+			
 		}
 
 
 		[TestMethod]
 		public void AddValidSentimentPositive1()
 		{
-			Sentiment sentiment = new Sentiment("I like it", "Positivo");
+			Sentiment sentiment = new Sentiment("I like it", Sentiment.sentimentType.Positive);
 
 			
 		
@@ -34,7 +37,7 @@ namespace Test
 		public void AddValidSentimentPositive2()
 		{
 
-			Sentiment sentiment = new Sentiment("Love", "Positivo");
+			Sentiment sentiment = new Sentiment("Love", Sentiment.sentimentType.Positive);
 
 			
 
@@ -48,11 +51,11 @@ namespace Test
 		[TestMethod]
 		public void VerifySentimentContained1()
 		{
-			Sentiment sentiment = new Sentiment("Me     gusta", "Positivo");
+			Sentiment sentiment = new Sentiment("Me     gusta", Sentiment.sentimentType.Positive);
 			
 			manegement.AddSentiment(sentiment);
 
-			Sentiment sentiment2 = new Sentiment("No       me         gusta", "Negativo");
+			Sentiment sentiment2 = new Sentiment("No       me         gusta", Sentiment.sentimentType.Negative);
 			manegement.AddSentiment(sentiment2); 
 
 		}
@@ -61,11 +64,11 @@ namespace Test
 		[TestMethod]
 		public void VerifySentimentContained2()
 		{
-			Sentiment sentiment = new Sentiment("No                                me                                              gusta", "Negativo");
+			Sentiment sentiment = new Sentiment("No                                me                                              gusta", Sentiment.sentimentType.Negative);
 			
 			manegement.AddSentiment(sentiment);
 
-			Sentiment sentiment2 = new Sentiment("Me            gusta", "Positivo");
+			Sentiment sentiment2 = new Sentiment("Me            gusta", Sentiment.sentimentType.Positive);
 			
 			manegement.AddSentiment(sentiment2);
 
@@ -75,11 +78,11 @@ namespace Test
 		[TestMethod]
 		public void VerifySentimentNotContained1()
 		{
-			Sentiment sentiment = new Sentiment("Me gusta", "Positivo");
+			Sentiment sentiment = new Sentiment("Me gusta", Sentiment.sentimentType.Positive);
 			
 			manegement.AddSentiment(sentiment);
 
-			Sentiment sentiment2 = new Sentiment("Me encanta", "Positivo");
+			Sentiment sentiment2 = new Sentiment("Me encanta", Sentiment.sentimentType.Positive);
 			
 			manegement.AddSentiment(sentiment2);
 
@@ -90,11 +93,11 @@ namespace Test
 		[TestMethod]
 		public void VerifySentimentNotContained2()
 		{
-			Sentiment sentiment = new Sentiment("Me        gusta mucho                ","Positivo");
+			Sentiment sentiment = new Sentiment("Me        gusta mucho                ",Sentiment.sentimentType.Positive);
 			
 			manegement.AddSentiment(sentiment);
 
-			Sentiment sentiment2 = new Sentiment("                     Me encanta                            ", "Positivo");
+			Sentiment sentiment2 = new Sentiment("                     Me encanta                            ", Sentiment.sentimentType.Positive);
 			
 			manegement.AddSentiment(sentiment2);
 
@@ -106,7 +109,7 @@ namespace Test
 		[TestMethod]
 		public void NotAddInvalidadSentimentPositive1()
 		{
-			Sentiment sentiment = new Sentiment("                               ", "Neutro");
+			Sentiment sentiment = new Sentiment("                               ", Sentiment.sentimentType.Neutral);
 			manegement.AddSentiment(sentiment); 
 		}
 
@@ -116,7 +119,7 @@ namespace Test
 		[ExpectedException(typeof(ArgumentNullException))]
 		public void NotAddInvalidSentimentPositive2()
 		{
-			Sentiment sentiment = new Sentiment("", "Neutro");
+			Sentiment sentiment = new Sentiment("", Sentiment.sentimentType.Neutral);
 			
 			manegement.AddSentiment(sentiment); 
 
@@ -125,7 +128,7 @@ namespace Test
 		[TestMethod]
 		public void DeleteSentimentPositive()
 		{
-			Sentiment sentiment = new Sentiment("Me gusta", "Positivo");
+			Sentiment sentiment = new Sentiment("Me gusta", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
 			manegement.DeleteText(sentiment);
 			Assert.IsTrue(manegement.IsEmpty()); 
@@ -136,10 +139,10 @@ namespace Test
 		[TestMethod]
 		public void DeleteThreeSentimentPositive1()
 		{
-			Sentiment sentiment = new Sentiment("Me gusta","Positivo");
-			Sentiment sentiment2 = new Sentiment("Me encanta", "Positivo");
-			Sentiment sentiment3 = new Sentiment("Lo amo", "Positivo");
-			Sentiment sentiment4 = new Sentiment("Es precioso", "Positivo");
+			Sentiment sentiment = new Sentiment("Me gusta",Sentiment.sentimentType.Positive);
+			Sentiment sentiment2 = new Sentiment("Me encanta", Sentiment.sentimentType.Positive);
+			Sentiment sentiment3 = new Sentiment("Lo amo", Sentiment.sentimentType.Positive);
+			Sentiment sentiment4 = new Sentiment("Es precioso", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
 			manegement.AddSentiment(sentiment2);
 			manegement.AddSentiment(sentiment3);
@@ -155,10 +158,10 @@ namespace Test
 		[TestMethod]
 		public void DeleteThreeSentimentPositive2()
 		{
-			Sentiment sentiment = new Sentiment("Me gusta", "Positivo");
-			Sentiment sentiment2 = new Sentiment("Me encanta", "Positivo");
-			Sentiment sentiment3 = new Sentiment("Lo amo", "Positivo");
-			Sentiment sentiment4 = new Sentiment("Es precioso", "Positivo");
+			Sentiment sentiment = new Sentiment("Me gusta", Sentiment.sentimentType.Positive);
+			Sentiment sentiment2 = new Sentiment("Me encanta", Sentiment.sentimentType.Positive);
+			Sentiment sentiment3 = new Sentiment("Lo amo", Sentiment.sentimentType.Positive);
+			Sentiment sentiment4 = new Sentiment("Es precioso", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
 			manegement.AddSentiment(sentiment2);
 			manegement.AddSentiment(sentiment3);
@@ -175,7 +178,7 @@ namespace Test
 		[ExpectedException(typeof(TextManagementException))]
 		public void DeleteNotExistSentiment()
 		{
-			Sentiment sentiment = new Sentiment("Me gusta", "Positivo");
+			Sentiment sentiment = new Sentiment("Me gusta", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
 			manegement.DeleteText(sentiment);
 			manegement.DeleteText(sentiment); 
@@ -186,9 +189,9 @@ namespace Test
 		[ExpectedException(typeof(TextManagementException))]
 		public void DeleteNotExistSentiment2()
 		{
-			Sentiment sentiment = new Sentiment("Me gusta", "Positivo");
+			Sentiment sentiment = new Sentiment("Me gusta", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
-			Sentiment sentiment2 = new Sentiment("Me encanta", "Positivo");
+			Sentiment sentiment2 = new Sentiment("Me encanta", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment2); 
 			manegement.DeleteText(sentiment);
 			manegement.DeleteText(sentiment2);
@@ -200,10 +203,10 @@ namespace Test
 		[ExpectedException(typeof(ArgumentNullException))]
 		public void TryingDeleteEmptySentiment()
 		{
-			Sentiment sentiment = new Sentiment("", "Positivo");
+			Sentiment sentiment = new Sentiment("", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
 			manegement.DeleteText(sentiment);
-			Sentiment sentiment2 = new Sentiment("Me encanta", "Positivo");
+			Sentiment sentiment2 = new Sentiment("Me encanta", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment2);
 			manegement.DeleteText(sentiment2);
 
@@ -212,7 +215,7 @@ namespace Test
         [TestMethod]
         public void AddValidSentimentNegative1()
         {
-            Sentiment sentiment = new Sentiment("Lo odio", "Negativo");
+            Sentiment sentiment = new Sentiment("Lo odio", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
             Assert.IsFalse(manegement.IsEmpty());
         }
@@ -220,7 +223,7 @@ namespace Test
         [TestMethod]
         public void AddValidSentimentNegative2()
         {
-            Sentiment sentiment = new Sentiment("Detesto", "Negativo");
+            Sentiment sentiment = new Sentiment("Detesto", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
             Assert.IsFalse(manegement.IsEmpty());
         }
@@ -229,9 +232,9 @@ namespace Test
         [TestMethod]
         public void VerifyNegativeSentimentContained1()
         {
-            Sentiment sentiment = new Sentiment("No     me gusta", "Negativo");
+            Sentiment sentiment = new Sentiment("No     me gusta", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
-            Sentiment sentiment2 = new Sentiment("No me gusta para nada", "Negativo");
+            Sentiment sentiment2 = new Sentiment("No me gusta para nada", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment2);
         }
 
@@ -239,18 +242,18 @@ namespace Test
         [TestMethod]
         public void VerifyNegativeSentimentContained2()
         {
-            Sentiment sentiment = new Sentiment("No me gusta en absoluto", "Negativo");
+            Sentiment sentiment = new Sentiment("No me gusta en absoluto", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
-            Sentiment sentiment2 = new Sentiment("NO   ME GUSTA", "Negativo");
+            Sentiment sentiment2 = new Sentiment("NO   ME GUSTA", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment2);
         }
 
         [TestMethod]
         public void VerifyNegativeSentimentNotContained1()
         {
-            Sentiment sentiment = new Sentiment("Lo odio", "Negativo");
+            Sentiment sentiment = new Sentiment("Lo odio", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
-            Sentiment sentiment2 = new Sentiment("Lo detesto", "Negativo");
+            Sentiment sentiment2 = new Sentiment("Lo detesto", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment2);
             Assert.IsTrue(manegement.SentimentList.Count == 2);
         }
@@ -258,9 +261,9 @@ namespace Test
         [TestMethod]
         public void VerifyNegativeSentimentNotContained2()
         {
-            Sentiment sentiment = new Sentiment("  Lo  odio              ", "Negativo");
+            Sentiment sentiment = new Sentiment("  Lo  odio              ", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
-            Sentiment sentiment2 = new Sentiment("Yo    odio", "Negativo");
+            Sentiment sentiment2 = new Sentiment("Yo    odio", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment2);
             Assert.AreEqual("Lo odio", manegement.SentimentList[0].SentimientText);
         }
@@ -268,7 +271,7 @@ namespace Test
         [TestMethod]
         public void DeleteNegativeSentiment()
         {
-            Sentiment sentiment = new Sentiment("Es nefasto", "Negativo");
+            Sentiment sentiment = new Sentiment("Es nefasto", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
             manegement.DeleteText(sentiment);
             Assert.IsTrue(manegement.IsEmpty());
@@ -277,10 +280,10 @@ namespace Test
         [TestMethod]
         public void DeleteThreeNegativesSentiments1()
         {
-            Sentiment sentiment = new Sentiment("Lo odio", "Negativo");
-            Sentiment sentiment2 = new Sentiment("Lo detesto", "Negativo");
-            Sentiment sentiment3 = new Sentiment("Es nefasto", "Negativo");
-            Sentiment sentiment4 = new Sentiment("De lo peor", "Negativo");
+            Sentiment sentiment = new Sentiment("Lo odio", Sentiment.sentimentType.Negative);
+            Sentiment sentiment2 = new Sentiment("Lo detesto", Sentiment.sentimentType.Negative);
+            Sentiment sentiment3 = new Sentiment("Es nefasto", Sentiment.sentimentType.Negative);
+            Sentiment sentiment4 = new Sentiment("De lo peor", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
             manegement.AddSentiment(sentiment2);
             manegement.AddSentiment(sentiment3);
@@ -294,10 +297,10 @@ namespace Test
         [TestMethod]
         public void DeleteThreeNegativesSentiments2()
         {
-            Sentiment sentiment = new Sentiment("Lo odio", "Negativo");
-            Sentiment sentiment2 = new Sentiment("Lo detesto", "Negativo");
-            Sentiment sentiment3 = new Sentiment("Es nefasto", "Negativo");
-            Sentiment sentiment4 = new Sentiment("De lo peor", "Negativo");
+            Sentiment sentiment = new Sentiment("Lo odio", Sentiment.sentimentType.Negative);
+            Sentiment sentiment2 = new Sentiment("Lo detesto", Sentiment.sentimentType.Negative);
+            Sentiment sentiment3 = new Sentiment("Es nefasto", Sentiment.sentimentType.Negative);
+            Sentiment sentiment4 = new Sentiment("De lo peor", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
             manegement.AddSentiment(sentiment2);
             manegement.AddSentiment(sentiment3);
@@ -312,7 +315,7 @@ namespace Test
         [ExpectedException(typeof(TextManagementException))]
         public void DeleteNotExistNegativeSentiment()
         {
-            Sentiment sentiment = new Sentiment("Lo odio", "Negativo");
+            Sentiment sentiment = new Sentiment("Lo odio", Sentiment.sentimentType.Negative);
             manegement.AddSentiment(sentiment);
             manegement.DeleteText(sentiment);
             manegement.DeleteText(sentiment);
@@ -323,9 +326,9 @@ namespace Test
 		[ExpectedException(typeof(TextManagementException))]
 		public void TryingAddInvalidSentiment()
 		{
-			Sentiment sentiment = new Sentiment("Posible", "Positivo");
+			Sentiment sentiment = new Sentiment("Posible", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
-			Sentiment sentiment2 = new Sentiment("Imposible", "Negativo");
+			Sentiment sentiment2 = new Sentiment("Imposible", Sentiment.sentimentType.Negative);
 			manegement.AddSentiment(sentiment2); 
 		}
 
@@ -333,9 +336,9 @@ namespace Test
 		[ExpectedException(typeof(TextManagementException))]
 		public void TryingAddInvalidSentiment2()
 		{
-			Sentiment sentiment = new Sentiment("Imposible", "Negativo");
+			Sentiment sentiment = new Sentiment("Imposible", Sentiment.sentimentType.Negative);
 			manegement.AddSentiment(sentiment);
-			Sentiment sentiment2 = new Sentiment("Posible", "Positivo");
+			Sentiment sentiment2 = new Sentiment("Posible", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment2);
 		}
 
@@ -344,15 +347,23 @@ namespace Test
 		[ExpectedException(typeof(TextManagementException))]
 		public void TryingAddRepeteadSentiment()
 		{
-			Sentiment sentiment = new Sentiment("Posible", "Positivo");
+			Sentiment sentiment = new Sentiment("Posible", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment);
-			Sentiment sentiment2 = new Sentiment("Posible", "Positivo");
+			Sentiment sentiment2 = new Sentiment("Posible", Sentiment.sentimentType.Positive);
 			manegement.AddSentiment(sentiment2);
 		}
 
 
 
+		[TestMethod]
+		public void TryingToString()
+		{
+			Sentiment sentiment = new Sentiment("Me gusta", Sentiment.sentimentType.Positive);
+			Assert.AreEqual("Me gusta", sentiment.ToString()); 
+		}
 
+
+		
 
 
 
